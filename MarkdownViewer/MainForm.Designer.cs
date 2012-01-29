8c2ef70fbea6fb53f49ea0c7d904927f4c944a3b
Christof Jans <christofjans@gmail.com>
2012-01-29 14:12:28 -0500

    first project commit

---
+++ MarkdownViewer/MainForm.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace MarkdownViewer
+{
+  partial class MainForm
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.changeBtn = new System.Windows.Forms.Button();
+            this.pathLabel = new System.Windows.Forms.Label();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.fileSystemWatcher = new System.IO.FileSystemWatcher();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // changeBtn
+            // 
+            this.changeBtn.Location = new System.Drawing.Point(12, 12);
+            this.changeBtn.Name = "changeBtn";
+            this.changeBtn.Size = new System.Drawing.Size(75, 23);
+            this.changeBtn.TabIndex = 0;
+            this.changeBtn.Text = "Open";
+            this.changeBtn.UseVisualStyleBackColor = true;
+            this.changeBtn.Click += new System.EventHandler(this.changeBtn_Click);
+            // 
+            // pathLabel
+            // 
+            this.pathLabel.AutoSize = true;
+            this.pathLabel.Location = new System.Drawing.Point(93, 17);
+            this.pathLabel.Name = "pathLabel";
+            this.pathLabel.Size = new System.Drawing.Size(16, 13);
+            this.pathLabel.TabIndex = 1;
+            this.pathLabel.Text = "...";
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser.Location = new System.Drawing.Point(13, 42);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(668, 495);
+            this.webBrowser.TabIndex = 2;
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.Filter = "Text Files|*.txt";
+            // 
+            // fileSystemWatcher
+            // 
+            this.fileSystemWatcher.EnableRaisingEvents = true;
+            this.fileSystemWatcher.NotifyFilter = System.IO.NotifyFilters.LastWrite;
+            this.fileSystemWatcher.SynchronizingObject = this;
+            this.fileSystemWatcher.Changed += new System.IO.FileSystemEventHandler(this.fileSystemWatcher_Changed);
+            // 
+            // MainForm
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(693, 549);
+            this.Controls.Add(this.webBrowser);
+            this.Controls.Add(this.pathLabel);
+            this.Controls.Add(this.changeBtn);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Markdown Viewer";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.MainForm_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.MainForm_DragEnter);
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button changeBtn;
+    private System.Windows.Forms.Label pathLabel;
+    private System.Windows.Forms.WebBrowser webBrowser;
+    private System.Windows.Forms.OpenFileDialog openFileDialog;
+    private System.IO.FileSystemWatcher fileSystemWatcher;
+  }
+}
+
---
+++ MarkdownViewer/MainForm.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace MarkdownViewer
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Open()
+        {
+            TransformMarkdown();
+            webBrowser.Navigate("file://" + HtmlPath);
+            fileSystemWatcher.Path = Path.GetDirectoryName(path);
+            fileSystemWatcher.Filter = Path.GetFileName(path);
+            fileSystemWatcher.EnableRaisingEvents = true;
+        }
+        private void TransformMarkdown()
+        {
+            try
+            {
+                pathLabel.Text = path;
+                var text = File.ReadAllText(path);
+                var html = markdown.Transform(text);
+                html = string.Format(
+@"<html>
+    <head>
+        <link rel=""stylesheet"" type=""text/css"" href=""{0}"" />
+        <meta http-equiv=""X-UA-Compatible"" content=""IE=9"" >
+    </head>
+    <body>
+        {1}
+    </body>
+</html>", CssFileName, html);
+                File.WriteAllText(HtmlPath, html);
+                lastRefreshedTime = LastWriteTime;
+            }
+            catch (System.IO.IOException)
+            {
+                //sometimes we are too quick in accessing the files and it is still being used by other process
+            }
+        }
+        private void RefreshView()
+        {
+            TransformMarkdown();
+            webBrowser.Refresh();
+        }
+        private string GetPath(string extension)
+        {
+            var dir = Path.GetDirectoryName(path);
+            var fileName = Path.GetFileNameWithoutExtension(path);
+            return Path.Combine(dir, fileName + "." + extension);
+        }
+        private string HtmlPath
+        {
+            get { return GetPath("htm"); }
+        }
+        private DateTime LastWriteTime
+        {
+            get
+            {
+                return (new FileInfo(path)).LastWriteTime;
+            }
+        }
+        private string CssFileName
+        {
+            get
+            {
+                var fileName = Path.GetFileNameWithoutExtension(path);
+                return fileName + ".css";
+            }
+        }
+
+        private string path = null;
+        private MarkdownSharp.Markdown markdown = new MarkdownSharp.Markdown();
+        private DateTime lastRefreshedTime = new DateTime(1900, 1, 1);
+
+        private void changeBtn_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                path = openFileDialog.FileName;
+                Open();
+            }
+        }
+        private void fileSystemWatcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            if (LastWriteTime > lastRefreshedTime)
+                RefreshView();
+        }
+
+        private void MainForm_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent("FileName"))
+            {
+                path = ((string[])e.Data.GetData("FileNameW"))[0];
+                Open();
+            }
+        }
+
+        private void MainForm_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent("FileNameW"))
+                e.Effect = DragDropEffects.Link;
+        }
+    }
+}
