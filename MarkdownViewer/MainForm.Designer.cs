913d3d74d892b651ca1de870671e81ed259fb059
Christof Jans <christofjans@gmail.com>
2012-02-01 23:06:32 -0500

    added support for other file types

--- MarkdownViewer/MainForm.Designer.cs
+++ MarkdownViewer/MainForm.Designer.cs
@@ -28,73 +28,74 @@
     /// </summary>
     private void InitializeComponent()
     {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
-            this.changeBtn = new System.Windows.Forms.Button();
-            this.pathLabel = new System.Windows.Forms.Label();
-            this.webBrowser = new System.Windows.Forms.WebBrowser();
-            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
-            this.fileSystemWatcher = new System.IO.FileSystemWatcher();
-            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // changeBtn
-            // 
-            this.changeBtn.Location = new System.Drawing.Point(12, 12);
-            this.changeBtn.Name = "changeBtn";
-            this.changeBtn.Size = new System.Drawing.Size(75, 23);
-            this.changeBtn.TabIndex = 0;
-            this.changeBtn.Text = "Open";
-            this.changeBtn.UseVisualStyleBackColor = true;
-            this.changeBtn.Click += new System.EventHandler(this.changeBtn_Click);
-            // 
-            // pathLabel
-            // 
-            this.pathLabel.AutoSize = true;
-            this.pathLabel.Location = new System.Drawing.Point(93, 17);
-            this.pathLabel.Name = "pathLabel";
-            this.pathLabel.Size = new System.Drawing.Size(16, 13);
-            this.pathLabel.TabIndex = 1;
-            this.pathLabel.Text = "...";
-            // 
-            // webBrowser
-            // 
-            this.webBrowser.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+      this.changeBtn = new System.Windows.Forms.Button();
+      this.pathLabel = new System.Windows.Forms.Label();
+      this.webBrowser = new System.Windows.Forms.WebBrowser();
+      this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+      this.fileSystemWatcher = new System.IO.FileSystemWatcher();
+      ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // changeBtn
+      // 
+      this.changeBtn.Location = new System.Drawing.Point(12, 12);
+      this.changeBtn.Name = "changeBtn";
+      this.changeBtn.Size = new System.Drawing.Size(75, 23);
+      this.changeBtn.TabIndex = 0;
+      this.changeBtn.Text = "Open";
+      this.changeBtn.UseVisualStyleBackColor = true;
+      this.changeBtn.Click += new System.EventHandler(this.changeBtn_Click);
+      // 
+      // pathLabel
+      // 
+      this.pathLabel.AutoSize = true;
+      this.pathLabel.Location = new System.Drawing.Point(93, 17);
+      this.pathLabel.Name = "pathLabel";
+      this.pathLabel.Size = new System.Drawing.Size(16, 13);
+      this.pathLabel.TabIndex = 1;
+      this.pathLabel.Text = "...";
+      // 
+      // webBrowser
+      // 
+      this.webBrowser.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.webBrowser.Location = new System.Drawing.Point(13, 42);
-            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
-            this.webBrowser.Name = "webBrowser";
-            this.webBrowser.Size = new System.Drawing.Size(668, 495);
-            this.webBrowser.TabIndex = 2;
-            // 
-            // openFileDialog
-            // 
-            this.openFileDialog.Filter = "Text Files|*.txt";
-            // 
-            // fileSystemWatcher
-            // 
-            this.fileSystemWatcher.EnableRaisingEvents = true;
-            this.fileSystemWatcher.NotifyFilter = System.IO.NotifyFilters.LastWrite;
-            this.fileSystemWatcher.SynchronizingObject = this;
-            this.fileSystemWatcher.Changed += new System.IO.FileSystemEventHandler(this.fileSystemWatcher_Changed);
-            // 
-            // MainForm
-            // 
-            this.AllowDrop = true;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(693, 549);
-            this.Controls.Add(this.webBrowser);
-            this.Controls.Add(this.pathLabel);
-            this.Controls.Add(this.changeBtn);
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Name = "MainForm";
-            this.Text = "Markdown Viewer";
-            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.MainForm_DragDrop);
-            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.MainForm_DragEnter);
-            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+      this.webBrowser.Location = new System.Drawing.Point(13, 42);
+      this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+      this.webBrowser.Name = "webBrowser";
+      this.webBrowser.Size = new System.Drawing.Size(668, 495);
+      this.webBrowser.TabIndex = 2;
+      // 
+      // openFileDialog
+      // 
+      this.openFileDialog.Filter = "Markdown Files (*.txt;*.md;*.mdown;*.markdown)|*.txt;*.md;*.mdown;*.markdown|All " +
+    "Files (*.*)|*.*";
+      // 
+      // fileSystemWatcher
+      // 
+      this.fileSystemWatcher.EnableRaisingEvents = true;
+      this.fileSystemWatcher.NotifyFilter = System.IO.NotifyFilters.LastWrite;
+      this.fileSystemWatcher.SynchronizingObject = this;
+      this.fileSystemWatcher.Changed += new System.IO.FileSystemEventHandler(this.fileSystemWatcher_Changed);
+      // 
+      // MainForm
+      // 
+      this.AllowDrop = true;
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(693, 549);
+      this.Controls.Add(this.webBrowser);
+      this.Controls.Add(this.pathLabel);
+      this.Controls.Add(this.changeBtn);
+      this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+      this.Name = "MainForm";
+      this.Text = "Markdown Viewer";
+      this.DragDrop += new System.Windows.Forms.DragEventHandler(this.MainForm_DragDrop);
+      this.DragEnter += new System.Windows.Forms.DragEventHandler(this.MainForm_DragEnter);
+      ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher)).EndInit();
+      this.ResumeLayout(false);
+      this.PerformLayout();
 
     }
 
